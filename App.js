2e94ea4b13e1f36dfbfba34bff8243229b112ab8
mirlek <mirlenko.ekat@gmail.com>
2023-08-01 19:53:36 +0200

    Bookmarked toggle is ready

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 const bar = document.querySelector(".bar");
-const off = document.querySelector(".switcher_bookmarked_off");
-const on = document.querySelector(".switcher_bookmarked_on");
+document.querySelector(".switcher_bookmarked_off");
+document.querySelector(".switcher_bookmarked_on");
+const el =document.getElementById("switcher");
 
 setTimeout(() => {
   bar.style.setProperty("--progress", "80%");
 }, 50);
 
 function toggleClass() {
-    $(".switcher_bookmarked_off").toggleClass(".switcher_bookmarked_off");
+    el.classList.toggle("switcher_bookmarked_on");
   }
