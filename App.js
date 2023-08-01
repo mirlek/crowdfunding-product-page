3f8051d4ce816ab47576aa10ca0e266aeefeab36
mirlek <mirlenko.ekat@gmail.com>
2023-08-01 19:38:47 +0200

    Progress bar, Boormarked button correction

--- App.js
+++ App.js
@@ -1,5 +1,11 @@
 const bar = document.querySelector(".bar");
+const off = document.querySelector(".switcher_bookmarked_off");
+const on = document.querySelector(".switcher_bookmarked_on");
 
 setTimeout(() => {
   bar.style.setProperty("--progress", "80%");
 }, 50);
+
+function toggleClass() {
+    $(".switcher_bookmarked_off").toggleClass(".switcher_bookmarked_off");
+  }
